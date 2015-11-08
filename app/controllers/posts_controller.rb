cb64b25659565d353a8f0de288604e833ac3284d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-08 17:52:21 +0000

    Added model and migration for posts.  Added view for new post page.  Added form to view and create method to posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,22 @@
 class PostsController < ApplicationController
-    def index
-    end
+  def index
+    @post = Post.all
+  end
+    
+  def new
+  end
   
+  def create
+    # render plain: params[:posts].inspect
+    # p "-------------------params ----------------------"
+    # p params
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_url
+  end
+  
+  private
+    def post_params
+      params.require(:posts).permit(:link)
+    end
 end
