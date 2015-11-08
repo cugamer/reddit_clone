29e2ecb95afd87bc0c318088aef83060729e0730
Jonathon <jonathon.nordquist@gmail.com>
2015-11-08 20:43:24 +0000

    Add users resources to routes.  Add validations to User model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def create
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108193956) do
+ActiveRecord::Schema.define(version: 20151108202245) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "link"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151108193956) do
     t.datetime "updated_at",       null: false
     t.string   "descriptive_text"
     t.integer  "rating"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
