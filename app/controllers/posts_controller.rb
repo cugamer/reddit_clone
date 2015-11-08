765c89d2c376012acc2045baac667b4412a28671
Jonathon <jonathon.nordquist@gmail.com>
2015-11-08 18:34:48 +0000

    Add link_to methods to the post new and index templates linking the two together.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,11 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.create(post_params)
-    if @post
+    @post = Post.new(post_params)
+    if @post.save
       redirect_to root_url
     else
-      render new_url
+      render :new
     end
   end
   
