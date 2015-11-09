2a824b3ea35636f5e51a396c4cca59bc46ba0b8d
Jonathon <jonathon.nordquist@gmail.com>
2015-11-09 17:17:00 +0000

    Create log_out method in sessions controller and set to change @current_user to nil and delete the user_id from session.  Added call to log_out to sessions destroy method and redirect to root.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,6 +7,8 @@ module SessionsHelper
   # Method finds the current user using the session user_id.  This makes retreval
   # of the user possible on pages after login.  If there is already an @current_user
   # variable it's returned, else find_by finds the user ID and that's returned.
+  # The current_user variable is a "magic" Rails variable, don't need to worry
+  # about where it comes from or how it's managed at this level.
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -16,5 +18,12 @@ module SessionsHelper
   # Method returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+  
+  # Removes the user id from session and sets the @current_user variable to nil.
+  # This destroys the session.
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
