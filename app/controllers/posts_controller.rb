3dc410134ddef6d57fe7545bfef0434db6c474af
Jonathon <jonathon.nordquist@gmail.com>
2015-11-09 21:51:33 +0000

    Add method logged_in_user to posts controller to redirected non logged in users to login page.  Set as before filter to new and create methods in the post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: [:new, :create]
+  
   def index
     @post = Post.all
   end
@@ -18,5 +20,12 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:posts).permit(:link, :descriptive_text)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in to post."
+        redirect_to login_url
+      end
     end
 end
