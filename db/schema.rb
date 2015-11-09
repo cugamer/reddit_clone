946b7b1983ca0dc068e4bc9a298782492cdb4ba8
Jonathon <jonathon.nordquist@gmail.com>
2015-11-09 23:09:41 +0000

    Add migration to create comments table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109215438) do
+ActiveRecord::Schema.define(version: 20151109230531) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "comment_text"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "link"
