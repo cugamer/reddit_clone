0bc967181b6c2eb7e32ad58327c16418066eda2f
Jonathon <jonathon.nordquist@gmail.com>
2015-11-10 02:42:07 +0000

    Update field values for form in post show view.  Added save functionality to comment create method.

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -3,10 +3,19 @@ class CommentController < ApplicationController
   end
   
   def create
+    comment = Comment.new(comment_params)
+    p "------------------------#{comment_params}------------------------------"
+    p comment_params[:id]
+    if comment.save
+      flash[:success] = "Comment posted"
+    else
+      flash[:danger] = "Comment not posted"
+    end
+    redirect_to post_path(3)
   end
   
   private
     def comment_params
-      params.require(:comments).permit(:comment_body)
+      params.require(:comments).permit(:comment_text, :post_id)
     end
 end
